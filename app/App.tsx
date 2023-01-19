2976b683dda1ffa83992be0d819f2163d63aebab
Steph Hoel <steph.hoel@gmail.com>
2023-01-19 12:51:41 -0300

    Componente Header criado

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import { View, StatusBar } from 'react-native';
+import { StatusBar } from 'react-native';
 
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter'
-
-import { Styles } from './src/assets/global';
 
 import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
@@ -21,10 +18,10 @@ export default function App() {
     )
   }
   return (
-    <View style={Styles.container}>
+    <>
       <Home />
       <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
-    </View>
+    </>
   );
 }
 
--- app/src/screens/Home.tsx
+++ app/src/screens/Home.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import { View, Text } from "react-native";
 
 import { Styles } from '../assets/global';
+import { Header } from "../components/Header";
 
 export function Home() {
    return (
-      <View>
+      <View className="flex-1 bg-background px-8 pt-16">
+         <Header />
          <Text style={Styles.text}>Hallo Freunde!</Text>
       </View>
    )
